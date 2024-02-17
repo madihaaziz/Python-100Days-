996894a447c258edeb0f5261abb2556ddfa0423b
madihaaziz <126233391+madihaaziz@users.noreply.github.com>
2024-02-17 14:50:03 +0000

    1

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello World \nand goodbye world") #Escape sequence
+#BASICS OF PYTHON
+# print("Hello World \"and\" goodbye world") 
+#Escape sequence (control +back slash) and shift + end to select 
+# 
+# print("hey",6,7,sep="~",end="009\n")
+
+# print("Madiha")
+# a=1
+# c=True
+# d=None
+# e=1.1
+# f=complex(8,2)
+# print(a)
+# b="Madiha"
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+#list is mutable(changable)
+list1=[1,2.3,[-4,5],"apple","banana"]
+print(list1)
+
+#dictionary is key value pairs
+dict={"name":"Madiha","age":20,"conVote":True}
+print(dict)
+
