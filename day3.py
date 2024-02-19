7fc511fe3a5a032fb454ed8ac8cb3520a0387579
madihaaziz <126233391+madihaaziz@users.noreply.github.com>
2024-02-19 23:57:40 +0000

    typecasting..

---
+++ day3.py
@@ -0,0 +1,13 @@
+# Typecasting
+# Python converts smaller data type to higher data type to prevent data loss
+
+#Explicit Typecasting
+a="1"
+b="2"
+print(a+b)
+print(int(a)+int(b))
+
+#Implicit Typecasting
+a=1.9
+b=8  #int to float
+print(a+b)
