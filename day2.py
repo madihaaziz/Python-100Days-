1fc264c4d2c162ace7218347c44b972abba4c7a0
madihaaziz <126233391+madihaaziz@users.noreply.github.com>
2024-02-19 23:37:32 +0000

    Calculator Basic Program

--- day2.py
+++ day2.py
@@ -1,1 +1,11 @@
-print("a")
+#Calculator
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Modulo: ",a%b)
+print("Division: ",a/b)
+print("Floor Divion: ",a//b)
+
